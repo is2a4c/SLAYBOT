12bac3f75df82f3446bf938f08d6700bc5c1ca11
Павел Британ <britanpavel65@gmail.com>
2026-06-03 13:24:37 +0300

    Remove tracked local artifacts and fix CI config check

--- bot-proxy.js
+++
@@ -1,9 +0,0 @@
-// undici-proxy.js - Sets up proxy for all undici requests (used by discord.js)
-const { setGlobalDispatcher, ProxyAgent } = require('undici');
-
-// Set global proxy for undici
-const proxyAgent = new ProxyAgent('http://127.0.0.1:12334');
-setGlobalDispatcher(proxyAgent);
-
-// Now require the bot
-require('./bot');
--- replit.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodejs-24_x
-    pkgs.nodePackages.npm
-  ];
-}
